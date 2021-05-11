9ca695993d786db7e9d56ce021743b8c2eb1a4b3
James O'Halloran <james.p.ohalloran@gmail.com>
2021-05-11 15:42:52 -0300

    Update next.config.js
    use local starter

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-
+  env: {
+    IDENTITY_API_OVERRIDE:
+      "https://fh025fcred.execute-api.us-east-1.amazonaws.com/local",
+    CONTENT_API_OVERRIDE:
+      "https://q922ege2xh.execute-api.us-east-1.amazonaws.com",
+    BASE_TINA_URL: "james1.tinajs.dev",
+  },
 };
